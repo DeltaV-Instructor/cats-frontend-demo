0a4d9a33098337edeba7b873607e0f5d0803415d
tektechnologies <info@tektechnologies.com>
2023-02-17 12:56:27 -0600

    add cat components update and delete

--- src/index.js
+++ src/index.js
@@ -3,12 +3,32 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import About from './components/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route
+} from "react-router-dom";
+
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={ <App /> }>
+      <Route path="/about" element={ <About /> } />
+    </Route>
+  )
+);
+
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={ router } />
   </React.StrictMode>
 );
 
