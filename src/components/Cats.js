e3d217b985f54d8027f1d7fcfddce1c5e1de9710
tektechnologies <info@tektechnologies.com>
2023-02-17 16:06:07 -0600

    add import and format

--- src/components/Cats.js
+++ src/components/Cats.js
@@ -1,57 +1,57 @@
 import React from "react";
 import { Container, ListGroup, Button } from "react-bootstrap";
-
+import UpdateCatForm from "./UpdateCatForm.js";
 
 class Cats extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       showUpdateForm: false,
-    }
+    };
   }
 
-  render(){
+  render() {
     console.log(this.props.cats);
     let cats = this.props.cats.map((cat) => (
-      <Cat 
-      cat={cat} 
-      key={cat._id} 
-      deleteCats={this.props.deleteCats}
-      updateCats={this.props.updateCats}  
+      <Cat
+        cat={cat}
+        key={cat._id}
+        deleteCats={this.props.deleteCats}
+        updateCats={this.props.updateCats}
       />
-    
     ));
-    return(
-     <Container>
-      <ListGroup>{cats}</ListGroup>
-     </Container>
+    return (
+      <Container>
+        <ListGroup>{cats}</ListGroup>
+      </Container>
     );
   }
 }
 
 //two components in the same file they belong together and Cats can only use Cat
 class Cat extends Cats {
-
-  render(){
-    return(
+  render() {
+    return (
       <>
-      <ListGroup.Item>
-        {this.props.cat.name} is {this.props.cat.color}
-        <Button 
-         variant="warning"
-         onClick={() => this.props.deleteCats(this.props.cat._id)}>Delete Cat</Button>
-         <Button 
-         variant="success"
-         onClick={() => this.state({showUpdateForm: true})}>Update Cat</Button>
-      </ListGroup.Item>
-      {
-        this.state.showUpdateForm &&
-        <UpdateCatForm  cat={this.props.cat}/>
-      }
+        <ListGroup.Item>
+          {this.props.cat.name} is {this.props.cat.color}
+          <Button
+            variant="warning"
+            onClick={() => this.props.deleteCats(this.props.cat._id)}
+          >
+            Delete Cat
+          </Button>
+          <Button
+            variant="success"
+            onClick={() => this.state({ showUpdateForm: true })}
+          >
+            Update Cat
+          </Button>
+        </ListGroup.Item>
+        {this.state.showUpdateForm && <UpdateCatForm cat={this.props.cat} />}
       </>
     );
   }
 }
-
 
 export default Cats;
